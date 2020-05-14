9077249b920bfdea172de8a00e0237388128f7e8
林朝阳 <3359414533@qq.com>
2020-05-14 10:48:29 +0800

    郭建豪的秒杀系统

---
+++ src/main/java/com/imooc/miaosha/result/CodeMsg.java
@@ -0,0 +1,38 @@
+package com.imooc.miaosha.result;
+
+public class CodeMsg {
+	
+	private int code;
+	private String msg;
+
+	private CodeMsg( int code,String msg ) {
+		this.code = code;
+		this.msg = msg;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public CodeMsg fillArgs(Object... args) {
+		int code = this.code;
+		String message = String.format(this.msg, args);
+		return new CodeMsg(code, message);
+	}
+
+	@Override
+	public String toString() {
+		return "CodeMsg [code=" + code + ", msg=" + msg + "]";
+	}
+	
+	
+}
